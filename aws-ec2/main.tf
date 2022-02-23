13fff2138c382eb4b57f00a50ffb6d480a80bbe4
Maor Hazan <maorhz@gmail.com>
2022-02-23 23:17:00 +0200

    commiy

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "test-instance" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
